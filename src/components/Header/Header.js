ce5c8edf8427597e40f2df737df25fe944715a4b
unknown <nikita.samsonov@simbirsoft.com>
2022-03-31 13:00:14 +0300

    style like filter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Header = ({likeFilter}) => {
+    const likeFilterOpen = useSelector(state => state.main.filter)
   return (
         <div className="header">
-            <div className="likeFilter" onClick={() => likeFilter()}>
+            <div className={likeFilterOpen ? "likeFilter--open" : "likeFilter"} onClick={() => likeFilter()}>
                 <img src="images/like.png" alt="" />
             </div>
         </div>
